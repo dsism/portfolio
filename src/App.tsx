975d85f33e08360c6b4bd739087fdc422ae8d14f
Danielle Ismerim <danismerim@hotmail.com>
2024-04-07 15:09:40 -0300

    updating content

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,20 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
-function App() {
-  const [count, setCount] = useState(0)
-
+export const App = () => {
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <div className="main-container">
+      <div className='grid-parent'>
+        <div className='grid-portion grid-text'>
+          <h1>Full Stack Developer</h1>
+          <p>Welcome! I'm Danielle Ismerim, a web developer skilled in technologies built on top of JavaScript, such as React, NodeJS, TypeScript, etc. While this portfolio is under development, feel free to visit my LinkedIn profile for the latest update:</p>
+          <a target='_blank' href="https://www.linkedin.com/in/danielle-ismerim-743181101/">
+            <img className='icon' src="/linkedin.png" alt="linkedin" />
+          </a>
+        </div>
+        <div className='grid-portion grid-img'>
+          <img className='photo' alt='Danielle Ismerim - Full Stack Developer' src='/photo.jpeg' />
+        </div>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    </div>
   )
 }
-
-export default App
