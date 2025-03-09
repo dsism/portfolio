17a782f52d8157f0d6f68e6f414eca8d5aa45024
Danielle Ismerim <danismerim@hotmail.com>
2025-03-09 15:38:29 +0100

    removing gitlab link

--- src/App.tsx
+++ src/App.tsx
@@ -8,15 +8,15 @@ export const App = () => {
           <div>
             <h1>Full Stack Developer</h1>
             <p>Welcome! I'm Danielle Ismerim, a web developer skilled in technologies built on top of JavaScript, such as React, NodeJS, TypeScript, etc. </p>
-            <p>While this portfolio is under development, feel free to visit my LinkedIn profile for the latest updates or check out my GitLab for some code I've created for study purposes:</p>
+            <p>While this portfolio is under development, feel free to visit my LinkedIn profile for the latest updates by clicking the link below.</p>
           </div>
           <div className='icons-area'>
-            <a target='_blank' href="https://www.linkedin.com/in/danielle-ismerim-743181101/">
+            <a target='_blank' href="https://www.linkedin.com/in/danielleismerim/">
               <img className='icon' src="linkedin.png" alt="linkedin" />
             </a>
-            <a target='_blank' href="https://gitlab.com/danismerim">
+            {/* <a target='_blank' href="https://gitlab.com/danismerim">
               <img className='icon' src="gitlab.png" alt="gitlab" />
-            </a>
+            </a> */}
           </div>
         </div>
         <div className='grid-portion grid-img'>
