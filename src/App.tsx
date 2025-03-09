e53a26b69d050f6ef45c5ae96dc745e56b4daaf9
Danielle Ismerim <danismerim@hotmail.com>
2025-03-09 15:47:31 +0100

    adding effect to 'Welcome'

--- src/App.tsx
+++ src/App.tsx
@@ -1,28 +1,85 @@
-import './App.css'
+import { useEffect, useState } from "react";
+import "./App.css";
 
 export const App = () => {
+  const [typedText, setTypedText] = useState("");
+  const [isDeleting, setIsDeleting] = useState(false);
+  const [index, setIndex] = useState(0);
+  const [shouldDelete, setShouldDelete] = useState(false);
+  const text = "Welcome!";
+
+  useEffect(() => {
+    const typingInterval = setInterval(
+      () => {
+        if (!isDeleting) {
+          if (index < text.length) {
+            setTypedText((prev) => prev + text.charAt(index));
+            setIndex((prevIndex) => prevIndex + 1);
+          } else if (index === text.length) {
+            // Wait 5 seconds before starting the deletion
+            setShouldDelete(true);
+          }
+        } else {
+          if (index > 0) {
+            setTypedText((prev) => prev.slice(0, -1));
+            setIndex((prevIndex) => prevIndex - 1);
+          } else {
+            setIsDeleting(false);
+            setShouldDelete(false); // Reset the delay state
+          }
+        }
+      },
+      isDeleting ? 100 : 200
+    );
+
+    // Handle the 5-second delay before starting to delete the text
+    if (shouldDelete) {
+      setTimeout(() => {
+        setIsDeleting(true); // Start deleting after 5 seconds
+      }, 5000); // 5000ms = 5 seconds
+    }
+
+    return () => clearInterval(typingInterval); // Cleanup the interval
+  }, [index, isDeleting, shouldDelete, text]);
+
   return (
     <div className="main-container">
-      <div className='grid-parent'>
-        <div className='grid-portion grid-text'>
+      <div className="grid-parent">
+        <div className="grid-portion grid-text">
           <div>
             <h1>Full Stack Developer</h1>
-            <p>Welcome! I'm Danielle Ismerim, a web developer skilled in technologies built on top of JavaScript, such as React, NodeJS, TypeScript, etc. </p>
-            <p>While this portfolio is under development, feel free to visit my LinkedIn profile for the latest updates by clicking the link below.</p>
+            <div className="typing-container">
+              <span className="typing-text">{typedText}</span>
+              <span className="cursor">_</span>
+            </div>
+            <p>
+              I'm Danielle Ismerim, a web developer skilled in technologies
+              built on top of JavaScript, such as React, NodeJS, TypeScript,
+              etc.
+            </p>
+            <p>
+              While this portfolio is under development, feel free to visit my
+              LinkedIn profile for the latest updates by clicking the link
+              below.
+            </p>
           </div>
-          <div className='icons-area'>
-            <a target='_blank' href="https://www.linkedin.com/in/danielleismerim/">
-              <img className='icon' src="linkedin.png" alt="linkedin" />
+          <div className="icons-area">
+            <a
+              target="_blank"
+              href="https://www.linkedin.com/in/danielleismerim/"
+            >
+              <img className="icon" src="linkedin.png" alt="linkedin" />
             </a>
-            {/* <a target='_blank' href="https://gitlab.com/danismerim">
-              <img className='icon' src="gitlab.png" alt="gitlab" />
-            </a> */}
           </div>
         </div>
-        <div className='grid-portion grid-img'>
-          <img className='photo' alt='Danielle Ismerim - Full Stack Developer' src='photo.jpg' />
+        <div className="grid-portion grid-img">
+          <img
+            className="photo"
+            alt="Danielle Ismerim - Full Stack Developer"
+            src="photo.jpg"
+          />
         </div>
       </div>
     </div>
-  )
-}
+  );
+};
